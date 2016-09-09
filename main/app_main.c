873888bdea53906ad05f8ae8b32814f735659161
Ivan Grokhotkov <ivan@espressif.com>
2016-09-09 14:31:15 +0800

    Add spi_flash_read/write/erase sample

--- main/app_main.c
+++ main/app_main.c
@@ -1,17 +1,55 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_spi_flash.h"
+#include <string.h>
+#include <stdlib.h>
 
 
-void pingTask(void *pvParameters)
+static uint32_t g_wbuf[SPI_FLASH_SEC_SIZE / 4];
+static uint32_t g_rbuf[SPI_FLASH_SEC_SIZE / 4];
+
+void readWriteTask(void *pvParameters)
 {
-    while (1) {
+    srand(0);
+
+    for (uint32_t base_addr = 0x100000; 
+         base_addr < 0x200000; 
+         base_addr += SPI_FLASH_SEC_SIZE) 
+    {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        printf("ping\n");
+        
+        printf("erasing sector %x\n", base_addr / SPI_FLASH_SEC_SIZE);
+        spi_flash_erase_sector(base_addr / SPI_FLASH_SEC_SIZE);
+
+        for (int i = 0; i < sizeof(g_wbuf)/sizeof(g_wbuf[0]); ++i) {
+            g_wbuf[i] = rand();
+        }
+
+        printf("writing at %x\n", base_addr);
+        spi_flash_write(base_addr, g_wbuf, sizeof(g_wbuf));
+
+        memset(g_rbuf, 0, sizeof(g_rbuf));
+        printf("reading at %x\n", base_addr);
+        spi_flash_read(base_addr, g_rbuf, sizeof(g_rbuf));
+        for (int i = 0; i < sizeof(g_rbuf)/sizeof(g_rbuf[0]); ++i) {
+            if (g_rbuf[i] != g_wbuf[i]) {
+                printf("failed writing or reading at %d\n", base_addr + i * 4);
+                printf("got %08x, expected %08x\n", g_rbuf[i], g_wbuf[i]);
+                return;
+            }
+        }
+
+        printf("done at %x\n", base_addr);
+        base_addr += SPI_FLASH_SEC_SIZE;
     }
+
+    printf("read/write/erase test done\n");
+    vTaskDelete(NULL);
 }
+
 
 void app_main()
 {
-    xTaskCreatePinnedToCore(&pingTask, "pingTask", 2048, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(&readWriteTask, "readWriteTask", 2048, NULL, 5, NULL, 0);
 }
